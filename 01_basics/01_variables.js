eb1bbabbb92c4a88ff14b5d5764887c08b522457
Moyukh11 <moyukh0204@gmail.com>
2025-04-07 14:57:07 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -4,6 +4,10 @@ console.log(mail);
 console.log(id);
 mail="moyukh"
 var age=20
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
 city="howrah"
 console.log(mail)
 console.table([id,mail,city,age]);
