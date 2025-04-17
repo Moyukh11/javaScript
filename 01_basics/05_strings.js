b416e44addedcbd6d61e3be28d088c2ae7b911e3
Moyukh11 <moyukh0204@gmail.com>
2025-04-17 14:04:57 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,32 @@
+const name="moyukh"
+const repo=50
+console.log(`My name is ${name} and my repo count is ${repo}`);
+
+const name2 = new String ('moyukhdas')
+console.log(name2);
+console.log(name2.length);
+console.log(name2.toUpperCase());
+console.log(name2.substring(0,4));
+console.log(name2.slice(-8,5));
+console.log(name2.replace('d','-d'));
+console.log(name2.charAt(2));
+console.log(name2.indexOf('o'));
+
+const st="my name is moyukh das"
+console.log(st.split(' '));
+const st2=st.split(' ');
+console.log(st2[1]);
+console.log(st.includes('moyukh'));
+console.log(st.includes('roop'));
+
+
+
+
+
+
+
+
+
+
+
+
